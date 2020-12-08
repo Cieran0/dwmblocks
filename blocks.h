840d2f708b9cb085a5b97664fed58ee321a5fb33
Cieran0 <61164049+Cieran0@users.noreply.github.com>
2020-12-08 16:07:42 +0000

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -3,7 +3,6 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"🔊 ", /home/cieran/scripts/getVolume.sh,			10,		0},
 	
-	{"Cpu:", /home/cieran/scripts/cpuUsage.sh,			20,		0},
 	
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	20,		0},
 
