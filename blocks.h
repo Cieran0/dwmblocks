8bd8bd210ef3f83d2187d9b22e2df1cca91c4581
Cieran0 <jimmy121212121212121212@gmail.com>
2020-12-08 16:51:01 +0000

    Vol updates when changed

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"🔊 ", /home/cieran/scripts/getVolume.sh,			10,		0},
+	{"🔊 ", "/home/cieran/scripts/getVolume.sh",			0,		1},
 	
 	
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	20,		0},
