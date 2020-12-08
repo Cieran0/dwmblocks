39cddeaf3461a12b557f788c186e0f957e01df44
Cieran0 <61164049+Cieran0@users.noreply.github.com>
2020-12-08 09:06:12 +0000

    Added Volume

--- blocks.h
+++ blocks.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Cpu:", /home/cieran/scripts/cpuUsage.sh,			30,		0},	
+	{"🔊 ", /home/cieran/scripts/cpuUsage.sh,			10,		0},
 	
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"Cpu:", /home/cieran/scripts/getVolume.sh,			20,		0},
+	
+	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	20,		0},
 
 	{"", "date '+%H:%M'",					5,		0},
 };
